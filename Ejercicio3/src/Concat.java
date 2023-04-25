41c7a4d1e9415531c447f034423396b7f02ac8af
Dinael Aragon <84431313+DinaelAragonImaginArts@users.noreply.github.com>
2023-04-25 15:43:19 -0700

    Add files via upload

---
+++ Ejercicio3/src/Concat.java
@@ -0,0 +1,11 @@
+public class Concat {
+    public static void main(String[] args){
+        String[] texto = {"Uno","Dos", "Tres","Cuatro","Cinco","Seis"};
+        int palabras = texto.length;
+
+        for(int i = 0; i <= palabras; i++){
+            System.out.println(texto[i]);
+        }
+    }
+
+}
