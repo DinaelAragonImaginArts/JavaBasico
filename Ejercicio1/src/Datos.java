8a940f8b0ca178a5bb1a64daf82c9b1bf4978976
Dinael Aragon <84431313+DinaelAragonImaginArts@users.noreply.github.com>
2023-04-25 12:55:17 -0700

    Add files via upload

---
+++ Ejercicio1/src/Datos.java
@@ -0,0 +1,24 @@
+public class Datos {
+    public static void main(String[] args){
+        //1 Numericos
+
+        //1.1 Enteros
+        byte v1 = 5;
+        short v2 = 10;
+        int v3 = 30;
+        long v4 = 100;
+
+        //1.2 Decimales
+        float v5 = 5.5f;
+        double v6 = 10.5;
+        double precio = 49.99;
+        //2 booleano
+        boolean v7 = false;
+        boolean v8 = true;
+        //3 Texto
+        char v9 = 'a';
+        String v10 = "Lorem ipsum dolor sit amet.....";
+
+        System.out.println(v1 + "\n" + v2+ "\n"+v3+ "\n"+v4+ "\n"+v5+ "\n"+v6+ "\n"+precio+ "\n"+v7+ "\n"+v8+ "\n"+v9+ "\n"+v10+ "\n");
+    }
+}
